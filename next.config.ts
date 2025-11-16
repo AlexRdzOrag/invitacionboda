7a709015957b50f02ffdf3ad49ac9bd45413b473
Alejandro Rodriguez <lalolandino@gmail.com>
2025-11-16 15:50:22 -0600

    rendered as static

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your GitHub Pages repo is https://username.github.io/repo-name/
+  // uncomment and set basePath:
+  // basePath: '/repo-name',
 };
 
 export default nextConfig;
